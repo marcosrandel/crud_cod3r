f0fbd3476b2b5d09d4252efc5ee60a2c1c595fef
marcos <marcos.randel@agna.com.br>
2020-12-08 15:07:17 -0300

    First commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1', name: 'Maria', email: 'maria@alunis.com.br', avatarUrl: ''),
+  '2': const User(
+      id: '2', name: 'Rafael', email: 'rafael@alunos.com', avatarUrl: '')
+};
